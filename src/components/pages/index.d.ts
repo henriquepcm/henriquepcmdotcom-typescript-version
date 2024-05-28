f46e0fd021c8c50a97103e166c6f9997ae3aedd8
Henrique Pochmann <henriquepochmann@Henriques-MacBook-Pro.local>
2024-05-28 09:45:44 -0300

    adding Typescrip modules

---
+++ src/components/pages/index.d.ts
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+declare module "./components/pages" {
+     export const Home: FC;
+     export const About: FC;
+     export const Education: FC;
+     export const ProfessionalExperiences: FC;
+     export const Projects: FC;
+     export const Skills: FC;
+     export const PageNotFound: FC;
+     export const Certifications: FC;
+}
---
+++ src/components/ui/index.d.ts
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+declare module "./components/ui" {
+     export const Header: FC;
+     export const Footer: FC;
+     export const Menu: FC;
+}
