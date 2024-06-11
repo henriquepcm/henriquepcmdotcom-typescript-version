cb953d93830330de1c52a80c9ba4a9b7b4f39a02
Henrique Pochmann <henriquepochmann@Henriques-MacBook-Pro.local>
2024-06-11 11:47:35 -0300

    add a file apart for the About page data

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,5 +1,6 @@
 import Heading1 from "../ui/Heading1";
 import PageMainContent from "../ui/PageMainContent";
+import aboutData from "../../data/aboutData";
 
 function About(): JSX.Element {
      return (
@@ -7,32 +8,18 @@ function About(): JSX.Element {
                containerWrapper="container-wrapper-about"
                container="container-about"
           >
-               <Heading1 isHome={false}>About Henrique Pochmann</Heading1>
+               <Heading1 isHome={false}>{aboutData.h1}</Heading1>
                <div className="space-y-5">
+                    {aboutData.paragraphs.map((paragraph, index) => (
+                         <p key={index}>{paragraph}</p>
+                    ))}
                     <p>
-                         I'm Henrique Pochmann from Porto Alegre/RS - Brazil
-                         (GMT-3). With over 20 years of experience creating,
-                         developing, and participating in digital projects, and
-                         also with solid experience in design, I recently found
-                         my passion in front-end development. Currently, I'm
-                         fine-tuning my skills to move software engineering to
-                         the center of my professional life.
-                    </p>
-                    <p>
-                         Development aside, a few topics I am particularly
-                         interested in are investments, learning languages,
-                         music, and making pizza/burgers.
-                    </p>
-                    <p>
-                         Am I a good fit for your project? Feel free to drop me
-                         a line at
                          <a
+                              class="font-bold ml-1"
                               href="mailto:henriquepcm@gmail.com"
-                              className="font-bold ml-1"
                          >
                               henriquepcm@gmail.com
                          </a>
-                         &nbsp;and let's chat about it.
                     </p>
                </div>
           </PageMainContent>
---
+++ src/data/aboutData.tsx
@@ -0,0 +1,10 @@
+const aboutData = {
+     h1: "About Henrique Pochmann",
+     paragraphs: [
+          "I'm Henrique Pochmann from Porto Alegre/RS - Brazil (GMT-3). With over 20 years of experience creating, developing, and participating in digital projects, and also with solid experience in design, I recently found my passion in front-end development. Currently, I'm fine-tuning my skills to move software engineering to the center of my professional life.",
+          "Development aside, a few topics I am particularly interested in are investments, learning languages, music, and making pizza/burgers.",
+          "Am I a good fit for your project? Feel free to drop me a line at the email address below and let's chat about it.",
+     ],
+};
+
+export default aboutData;
